67721589a1ab048e729fd98b183d074deabe0ab5
YEYINTAUNG-21 <S10268975@connect.np.edu.sg>
2024-11-17 21:21:35 +0800

    header end

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let navbar = document.querySelector('.navbar');
+let navBar = document.querySelector('.navbar');
 
 document.querySelector('#menu-icon').onclick = () => {
-    navbar.classList.toggle('active');
+    navBar.classList.toggle('active');
 }
 
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-icon').onclick = () => {
+    cartItem.classList.toggle('active');
+}
+
+let searchBar = document.querySelector('.search-form');
+
+document.querySelector('#search-icon').onclick = () => {
+    searchBar.classList.toggle('active');
+}
