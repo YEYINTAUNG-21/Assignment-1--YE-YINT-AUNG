41ac6d352b49c96289d97a40f3c98a009ff04d20
YEYINTAUNG-21 <S10268975@connect.np.edu.sg>
2024-11-24 19:01:54 +0800

    customer section done

--- Home Page/script.js
+++ Home Page/script.js
@@ -15,3 +15,15 @@ let searchBar = document.querySelector('.search-form');
 document.querySelector('#search-icon').onclick = () => {
     searchBar.classList.toggle('active');
 }
+
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000, 
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
