9d8729f459abfbe17a0a78ef8acdcac3bb1a44b8
YEYINTAUNG-21 <S10268975@connect.np.edu.sg>
2024-11-24 22:15:45 +0800

    home page done

--- Home Page/script.js
+++ Home Page/script.js
@@ -1,8 +1,12 @@
+/* Menu */
+
 let navBar = document.querySelector('.navbar');
 
 document.querySelector('#menu-icon').onclick = () => {
     navBar.classList.toggle('active');
 }
+
+/* Cart */
 
 let cartItem = document.querySelector('.cart-items-container');
 
@@ -10,11 +14,15 @@ document.querySelector('#cart-icon').onclick = () => {
     cartItem.classList.toggle('active');
 }
 
+/* Search */
+
 let searchBar = document.querySelector('.search-form');
 
 document.querySelector('#search-icon').onclick = () => {
     searchBar.classList.toggle('active');
 }
+
+/* Slider */
 
 $(document).ready(function(){
     $('.testimonial-slider').slick({
@@ -26,4 +34,21 @@ $(document).ready(function(){
         fade: true,
         cssEase: 'linear'
     });
+});
+
+/* Subscribe message */
+
+document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const emailInput = document.querySelector('.newsletter-input');
+    if (emailInput.value !== '') {
+        const messageDiv = document.getElementById('subscriptionMessage');
+        messageDiv.style.display = 'block'; 
+        setTimeout(function() {
+            messageDiv.style.display = 'none'; 
+        }, 15000);
+        emailInput.value = ''; 
+    } else {
+        alert('Please enter your email.'); 
+    }
 });
